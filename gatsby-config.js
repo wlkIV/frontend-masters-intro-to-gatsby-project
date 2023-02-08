242e318f44634fe93a58c684841018ed3c9ce428
William Kann <kanndide@gmail.com>
2023-02-08 07:25:03 +0300

    added site metadata

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteMetadata: {
+        siteUrl: 'https://www.yourdomain.com',
+        title: 'Frontend Masters Intro to Gatsby',
+        description: 'Frontend Masters Intro to Gatsby course projects',
+        image: ''
+    },
+};
