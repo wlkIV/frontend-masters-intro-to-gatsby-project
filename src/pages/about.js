ee4100e16e0f57c1ee955246faa9b4a6982e311f
William Kann <kanndide@gmail.com>
2023-02-08 07:39:56 +0300

    added links to pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 export default function IAboutPage() {
     return (
         <main>
             <h1>About this site.</h1>
+            <Link to="/">Home</Link>
         </main>
     );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import * as React from 'react';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 
 export default function IndexPage() {
+    
+
     return (
         <main>
             <h1>Hello Frontend Masters!</h1>
+            <Link to="/about">About this site</Link>
         </main>
     );
 }
