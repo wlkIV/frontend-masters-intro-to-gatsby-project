3ecff32e4c51ce8a12c579167a7165e3152fe744
wlkIV <kanndide@gmail.com>
2023-02-09 07:32:14 +0300

    started mdx process, but versions of gatsby etc are not compatible with newer versions of plugins and started causing failures. This tutorial is a wrap.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,24 @@ module.exports = {
         description: 'Frontend Masters Intro to Gatsby course projects',
         image: ''
     },
-    plugins: ['gatsby-plugin-react-helmet'],
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'posts',
+                path: '${__dirname}/src/posts',
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-page-creator',
+            options: {
+                path: '${__dirname}/src/posts',
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-mdx',
+            options: {},
+        },
+    ],
 };
